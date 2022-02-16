6f759c67bca316aae7b11f91178cbcd00862fac5
KonradOstrowskii <97406457+KonradOstrowskii@users.noreply.github.com>
2022-02-16 14:58:05 +0100

    rock, paper , scissors GAME

---
+++ Rock,Paper,Scissors/RPS_GAME.py
@@ -0,0 +1,41 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+player = None
+
+
+while player not in choices:
+    player = input("rock ,paper or scissors? : ").lower()
+    if player == computer:
+        print("computer : ", computer)
+        print("player : ", player)
+        print("Tie")
+    elif player == "rock":
+        if computer == "paper":
+            print("computer : ", computer)
+            print("player : ", player)
+            print("Computer win")
+        if computer == "scissors":
+            print("computer : ", computer)
+            print("player : ", player)
+            print("Computer lose")
+    elif player == "paper":
+        if computer == "scissors":
+            print("computer : ", computer)
+            print("player : ", player)
+            print("Computer win")
+        if computer == "rock":
+            print("computer : ", computer)
+            print("player : ", player)
+            print("Computer lose")
+    elif player == "scissors":
+        if computer == "paper":
+            print("computer : ", computer)
+            print("player : ", player)
+            print("Computer win")
+        if computer == "paper":
+            print("computer : ", computer)
+            print("player : ", player)
+            print("Computer lose")
