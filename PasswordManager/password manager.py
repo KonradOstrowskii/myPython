c29f677d0666c831192726262a074742c05f3ff5
KonradOstrowskii <97406457+KonradOstrowskii@users.noreply.github.com>
2022-03-24 14:31:30 +0100

    passwordManager

--- PasswordManager/password manager.py
+++ PasswordManager/password manager.py
@@ -1,38 +1,55 @@
 
-from email.generator import DecodedGenerator
+from adminAccount import Message
+from adminAccount import Admin
 from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-plaintext = "*********".encode()
-token = fernet.encrypt(plaintext)
-output = token.decode()
-decrypted_token = fernet.decrypt(token)
-print(output)
 
 
 def add():
-    name = input ('Account Name : ')
-    pwd = input ('Password : ')
+    name = input('User Name : ')
+    pwd = input('Password : ')
+    key = pwd
+    key = Fernet.generate_key()
+    f = Fernet(key)
     with open('passwords.txt', 'a') as f:
-        f.write(name + '|' + str(decrypted_token) + "\n")
+        f.write(name + '|' + str(key) + "\n")
+    with open('decodePassword.txt', 'a') as file:
+        file.write(name + "|" + str(key) + '\n')
 
-def view ():
-     with open('passwords.txt', 'r') as f:
-         for line in f.readlines():
-           print(line.split())
-        
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: ", user, "###", "Password", passw)
+
+
+def view_decode_pwd():
+    with open('decodePassword.txt', 'r') as f:
+            for line in f.readlines():
+                data = line.rstrip()
+                user, passw = data.split("|")
+                print("User: ", user, "###", "Password", passw)
+                
+
 
 while True:
-    mode = input ("Would you like to add a new passwor or view existing ones(view,add),press q to quit : ")
+    mode = input("""Would you like to add a new password or view existing ones(view,add)
+or login into Admin account(admin),press q to quit : """.lower())
     if mode == "q":
-        break 
+        break
     if mode == "view":
         view()
+    elif mode == "admin":
+        Admin()
+        answer = input("Would You like see all password ?(y/n)".lower())
+        if answer == "y":
+            view_decode_pwd()
+        else :
+            break 
 
     elif mode == "add":
         add()
     else:
         print("Invalid mode.")
         continue
-
