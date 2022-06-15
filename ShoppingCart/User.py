5530a9259a123b2322aaa1df8af8df75fefcd250
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2022-06-15 16:08:24 +0200

    Update User.py

--- ShoppingCart/User.py
+++ ShoppingCart/User.py
@@ -8,17 +8,22 @@ class User():
         self.username = input("Enter username: ")
         self.password = input("Enter password: ")
         with open("UserRegister.txt", "r", encoding='utf-8') as file:
+            if "Username: " in file   == self.username and "Password: " in file == self.password:
+                print("login successful!")
+            else:
+                print("wrong password entered")
+
             if self.username == self.username and self.password == self.password:
                 print("login successful!")
+
         
 
     def register(self):
         self.username = input("Enter username: ")
         self.password = input("Enter password: ")
-        self.pass_check = input("Enter password: ")
+        self.pass_check = input("Confirm password: ")
         self.email = input("Enter email: ")
         with open("UserRegister.txt", "a", encoding='utf-8') as file:
                 file.write('Username:'+ self.username +' '+ 'Password:'+
                            self.password+' '+ 'email:'+ self.email)
-
     
