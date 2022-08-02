f764deabf4ed3cf6216cb1a1715767345394c296
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2022-08-02 14:36:25 +0200

    adding def color_maker()
    color_maker() is going to change Icon color depends on elevation of volcano

--- mapping/maping_volcanos.py
+++ mapping/maping_volcanos.py
@@ -1,3 +1,4 @@
+
 import folium
 import pandas as pd
 
@@ -8,6 +9,14 @@ elev =list(data["ELEV"])
 name = list(data["NAME"])
 vol = " Volcano"
 
+
+def color_maker(elevation):
+     if elevation <=999:
+          return "green"
+     elif 1000 <= elevation <=2000:
+          return "orange"
+     else:
+          return "red"
 html = """
 Volcano name:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
@@ -18,13 +27,10 @@ fg = folium.FeatureGroup(name="My Map")
 m.add_child(folium.Marker(location=[32.7356,-16.9289],popup="marker",icon=folium.Icon(color='green')))
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name +vol , name + vol, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
+
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = str(color_maker(el)))))
 
 m.add_child(fg)
 
 m.save("map.html")
-
-
-
-
 
