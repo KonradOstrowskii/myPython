50b11ae716f7bfd979d1be544c8d31fd7c52458e
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2022-08-03 08:36:54 +0200

    new_layer
    New layer that show name of country , area, population

--- mapping/maping_volcanos.py
+++ mapping/maping_volcanos.py
@@ -1,6 +1,7 @@
 
 import folium
 import pandas as pd
+import json
 
 data = pd.read_csv('Volcanoes.txt')
 lat = list(data["LAT"])
@@ -26,11 +27,25 @@ m = folium.Map(location=[38.7356,-102.9289],zoom_start=7,tiles = "Stamen Terrain
 fg = folium.FeatureGroup(name="My Map")
 m.add_child(folium.Marker(location=[32.7356,-16.9289],popup="marker",icon=folium.Icon(color='green')))
 for lt, ln, el, name in zip(lat, lon, elev, name):
-    iframe = folium.IFrame(html=html % (name +vol , name + vol, el), width=200, height=100)
-
+    iframe = folium.IFrame(html=html % (name + vol , name + vol, el), width=200, height=100)
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = str(color_maker(el)))))
 
+data1 = json.load(open('world.json', encoding='utf-8-sig'))
+
+fgp = folium.FeatureGroup(name = "Wrold Population")    
+
+fgp.add_child(folium.features.GeoJson(data=data1,  
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 20000000 else 'yellow' 
+if 20000000 <= x['properties']['POP2005'] < 50000000 else 'orange' if 50000000 <= x['properties']['POP2005'] < 100000000
+else 'red'}, 
+tooltip = folium.features.GeoJsonTooltip(fields=['NAME','AREA','POP2005'],
+aliases=['NAME:','AREA:','POPULATION:'],
+style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))))
+
+
 m.add_child(fg)
+m.add_child(fgp)
+m.add_child(folium.LayerControl())
+
 
 m.save("map.html")
-
